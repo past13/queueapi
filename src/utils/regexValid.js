074659559cdc8cd82bfac95aff9e8c20cc09ef6e
past13 <laurynas.janilionis@gmail.com>
2020-05-15 12:47:05 +0700

    add email validation

--- src/utils/regexValid.js
+++ src/utils/regexValid.js
@@ -1,2 +1,5 @@
 const validNumberRegex = RegExp(/^\d*[1-9]\d*$/)
 export default validNumberRegex
+
+const validateEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)
+export default validateEmailRegex
